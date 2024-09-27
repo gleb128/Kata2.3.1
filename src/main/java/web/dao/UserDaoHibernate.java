e8407895f0e36b8babc39ab640e15879de92ae3f
gleb128 <glebtide@gmail.com>
2024-09-27 18:40:15 +0300

    транзакции

--- src/main/java/web/dao/UserDaoHibernate.java
+++ src/main/java/web/dao/UserDaoHibernate.java
@@ -15,35 +15,34 @@ public class UserDaoHibernate implements Userdao {
     @PersistenceContext
     private EntityManager entityManager;
 
-    @Transactional
+
     @Override
     public void saveUser(User user) {
         entityManager.persist(user);
     }
 
 
-    @Transactional
     @Override
     public void deleteUser(Long id) {
         User user = entityManager.find(User.class, id);
         entityManager.remove(entityManager.contains(user) ? user : entityManager.merge(user));
     }
 
-    @Transactional
+
     @Override
     public User findUser(Long id) {
         User user = entityManager.find(User.class, id);
         return user;
     }
 
-    @Transactional
+
     @Override
     public List<User> findAllUsers() {
         List<User> users = entityManager.createQuery("from User", User.class).getResultList();
         return users;
     }
 
-    @Transactional
+
     @Override
     public void updateUser(User user) {
         entityManager.merge(user);
--- src/main/java/web/service/UserServiceImp.java
+++ src/main/java/web/service/UserServiceImp.java
@@ -2,6 +2,7 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import web.dao.Userdao;
 import web.model.User;
 
@@ -11,31 +12,37 @@ import java.util.List;
 public class UserServiceImp implements UserService {
     private Userdao userdao;
 
+
     @Autowired
     public UserServiceImp(Userdao userDao) {
         this.userdao = userDao;
     }
 
+    @Transactional
     @Override
     public void saveUser(User user) {
         userdao.saveUser(user);
     }
 
+    @Transactional
     @Override
     public void deleteUser(Long id) {
         userdao.deleteUser(id);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public User findUser(Long id) {
         return userdao.findUser(id);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<User> findAllUsers() {
         return userdao.findAllUsers();
     }
 
+    @Transactional
     @Override
     public void updateUser(User user) {
         userdao.updateUser(user);
